3ead9b094753ed58adcdb3cbf041a96e5c43ad4c
Chris Kemp <chris.kemp3000@gmail.com>
2017-06-18 12:51:01 +0100

    Added colour swatch

--- src/client/container/homepage.jsx
+++ src/client/container/homepage.jsx
@@ -13,9 +13,34 @@ class Homepage extends React.Component {
     documents: PropTypes.object
   }
   render() {
+    const colors = ['primary', 'secondary', 'tertiary', 'complementary'];
+    const shades = ['-darkest', '-darker', '', '-lighter', '-lightest'];
+    const style = { width: 80, textAlign: 'center' };
     return (
       <Template title='Homepage'>
         <p>Homepage</p>
+        <div id='colors'style={{ backgroundColor: 'white', borderRadius: 10, display: 'inline-block', padding: 10, margin: 10 }}>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                {shades.map(s => (
+                  <th key={s}>{s}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {colors.map(c => (
+                <tr key={c}>
+                  <th>{c}</th>
+                  {shades.map(s => (
+                    <td key={s} className={`${c}${s}-bg`} style={style}>x</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </Template>
     );
   }
